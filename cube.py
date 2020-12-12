52cd0c0449ff7358259a50a2f5fe205b3dab2a69
Jisoo Kim <ghis22130@gmail.com>
2020-12-12 14:58:47 +0900

    Create enterF method
    큐브의 한면인 Front를 90도 회전하는 메소드를 구현하였습니다.
    이를 구현하기 위해 setSideList, getLeftSide, getRightSide, sideRotaion, faceRotation,  setSide 함수를 추가로 작성하였습니다.
    배열 확인 상 회전을 확인 할 수 있었지만 출력시 적용이 되지 않는 issue가 존재함.

--- cube.py
+++ cube.py
@@ -4,6 +4,8 @@
 # In[]:
 
 
+import copy
+
 class Cube :
     #6면체 색상 셋팅
     def __init__ (self):
@@ -13,10 +15,75 @@ class Cube :
         self.up = [["B" for _ in range(3)] for _ in range(3)]
         self.down = [["R" for _ in range(3)] for _ in range(3)]
         self.back = [["Y" for _ in range(3)] for _ in range(3)]
+        self.myCube = [front,right,left,up,down,back]
     
+    def setSideList(self,side1,side2,side3,side4):
+        return side1+side2+side3+side4        
+    
+    def getLeftSide(self,face):
+        return [i[2] for i in face]
+    
+    def getRightSide(self,face):
+        return [i[0] for i in face]
+            
+    def sideRotation(self,ls,ds,rs,us):
+        side_list = self.setSideList(ls,ds,rs,us)
+        temp = copy.deepcopy(side_list)
+        for idx, i in enumerate(side_list):
+            side_list[idx] = temp[(idx+3)%len(side_list)]
+        
+        sl = [side_list[i * 3:(i + 1) * 3] for i in range((len(side_list) + 3 - 1) // 3 )] 
+
+        return sl
+        
+    def faceRotation(self,face):
+        N = len(face)
+        ret = [[0] * N for _ in range(N)]
+
+        for r in range(N):
+            for c in range(N):
+                ret[c][N-1-r] = face[r][c]
+        return ret
+                 
+    def setSide(self,face,col,sl):
+        if face == self.front :
+            for i in range(3) :
+                self.front[col][i] == sl[i]
+        elif face == self.left :
+            for i in range(3) :
+                self.left[col][i] == sl[i]
+        elif face == self.right :
+            for i in range(3) :
+                self.right[col][i] == sl[i]
+        elif face == self.up :
+            for i in range(3) :
+                self.up[col][i] == sl[i]
+        elif face == self.down :
+            for i in range(3) :
+                self.down[col][i] == sl[i]
+        elif face == self.back :
+            for i in range(3) :
+                self.back[col][i] == sl[i]
+            
+        
+    def enterF(self):
+        ls = self.getLeftSide(self.left)
+        rs = self.getRightSide(self.right)
+        us = self.up[0]
+        ds = self.down[0]
+
+        sl = self.sideRotation(ls,rs,us,ds)
+        
+        print(sl)
+        self.up[0] = sl[3]
+        self.down[0] = sl[1]
+        self.setSide(self.left,2,sl[0])
+        self.setSide(self.right,0,sl[2])
+        self.front = self.faceRotation(self.front)
+        
     #현재 큐브 상태 출력
     def printCube(self) :
-        for row in self.up :
+        for row in up :
             for _ in range(3):
                 print("    ",end="")
             for col in row :
@@ -25,12 +92,12 @@ class Cube :
 
     
         for i in range(3):
-            print(self.left[i][0],self.left[i][1],self.left[i][2],end="   ")
-            print(self.front[i][0],self.front[i][1],self.front[i][2],end ="   ")
-            print(self.right[i][0],self.right[i][1],self.right[i][2],end = "   ")
-            print(self.back[i][0],self.back[i][1],self.back[i][2])
+            print(left[i][0],left[i][1],left[i][2],end="   ")
+            print(front[i][0],front[i][1],front[i][2],end ="   ")
+            print(right[i][0],right[i][1],right[i][2],end = "   ")
+            print(back[i][0],back[i][1],back[i][2])
 
-        for row in self.down :
+        for row in down :
             for _ in range(3):
                 print("   ",end=" ")
             for col in row :
@@ -38,4 +105,45 @@ class Cube :
             print()
 
 cube = Cube()
-cube.printCube() 
+cube.printCube()
+
+isGoing = True
+while(isGoing):
+    msg = input("CUBE> ")    
+    for idx, i in enumerate(msg) :
+        if i =="'" : # ' 이면 아무런 동작 수행하지 않는다.
+            continue
+        if i == "Q" : # "Q" 입력시 실행 종료
+            isGoing=False 
+            print("bye~")
+            break
+        if idx != len(msg)-1 and  msg[idx+1] =="'" : #현재 위치가 입력 문자열에 맨 끝이 아니고 다음 문자에 ' 가 있는 경우
+            if i == "U":
+                cube.enterUr()
+            elif i == "R":
+                cube.enterRr()
+            elif i == "L":
+                cube.enterLr()
+            elif i == "B":
+                cube.enterBr()
+            elif i == "F":
+                cube.enterFr()
+            elif i == "D":
+                cube.enterDr()
+                
+            print(i+"'")
+        else :  #'를 수행하지 않는 입력
+            if i == "U":
+                cube.enterU()
+            elif i == "R":
+                cube.enterR()
+            elif i == "L":
+                cube.enterL()
+            elif i == "B":
+                cube.enterB()
+            elif i == "F":
+                cube.enterF()
+            elif i == "D":
+                cube.enterD()
+            print(i) #현재 수행한 명령을 출력
+    cube.printCube() #명령 수행 후 변환된 큐브 출력
