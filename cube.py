19169168e3cf68d2aaefc9b247146fb4ff795c67
Jisoo Kim <ghis22130@gmail.com>
2020-12-12 15:02:15 +0900

    Modify setSide, printCube
    printCube에서 print해줄 때 변수 앞 "self."이 부재했기 때문에 출력이 제대로 되지 않았습니다.
    setSide에서는 = 연산자가 == 로 선언이 되어 있어 side에 적절한 값이 할당되지 않았습니다.

--- cube.py
+++ cube.py
@@ -17,15 +17,17 @@ class Cube :
         self.back = [["Y" for _ in range(3)] for _ in range(3)]
         self.myCube = [front,right,left,up,down,back]
     
+    # 회전해야할 옆면들을 하나의 배열로 묶어줍니다
     def setSideList(self,side1,side2,side3,side4):
         return side1+side2+side3+side4        
     
+    #회전하고자 하는 면의 왼쪽 옆면을 반환
     def getLeftSide(self,face):
         return [i[2] for i in face]
-    
+    #회전하고자 하는 면의 오른쪽 옆면을 반환
     def getRightSide(self,face):
         return [i[0] for i in face]
-            
+    #회전하고자 하는 옆면의 정보를 하나의 리스트로 담아 회전시켜준다음 다시 분리해서 반환 (왼쪽 아래 오른쪽 위) 순서
     def sideRotation(self,ls,ds,rs,us):
         side_list = self.setSideList(ls,ds,rs,us)
         temp = copy.deepcopy(side_list)
@@ -33,9 +35,9 @@ class Cube :
             side_list[idx] = temp[(idx+3)%len(side_list)]
         
         sl = [side_list[i * 3:(i + 1) * 3] for i in range((len(side_list) + 3 - 1) // 3 )] 
-
         return sl
-        
+    
+    #회전하고자 하는 면을 시계방향으로 90도 회전
     def faceRotation(self,face):
         N = len(face)
         ret = [[0] * N for _ in range(N)]
@@ -44,28 +46,28 @@ class Cube :
             for c in range(N):
                 ret[c][N-1-r] = face[r][c]
         return ret
-                 
+    #회전된 옆면을 적용
     def setSide(self,face,col,sl):
-        if face == self.front :
+        if face == "front" :
             for i in range(3) :
-                self.front[col][i] == sl[i]
-        elif face == self.left :
+                self.front[col][i] = sl[i]
+        elif face == "left" :
             for i in range(3) :
-                self.left[col][i] == sl[i]
-        elif face == self.right :
+                self.left[col][i] = sl[i]
+        elif face == "right" :
             for i in range(3) :
-                self.right[col][i] == sl[i]
-        elif face == self.up :
+                self.right[col][i] = sl[i]
+        elif face == "up" :
             for i in range(3) :
-                self.up[col][i] == sl[i]
-        elif face == self.down :
+                self.up[col][i] = sl[i]
+        elif face == "down" :
             for i in range(3) :
-                self.down[col][i] == sl[i]
-        elif face == self.back :
+                self.down[col][i] = sl[i]
+        elif face == "back" :
             for i in range(3) :
-                self.back[col][i] == sl[i]
+                self.back[col][i] = sl[i]
             
-        
+    #F입력받았을때
     def enterF(self):
         ls = self.getLeftSide(self.left)
         rs = self.getRightSide(self.right)
@@ -74,30 +76,29 @@ class Cube :
 
         sl = self.sideRotation(ls,rs,us,ds)
         
-        print(sl)
         self.up[0] = sl[3]
         self.down[0] = sl[1]
-        self.setSide(self.left,2,sl[0])
-        self.setSide(self.right,0,sl[2])
+        self.setSide("left",2,sl[0])
+        self.setSide("right",0,sl[2])
         self.front = self.faceRotation(self.front)
         
     #현재 큐브 상태 출력
     def printCube(self) :
-        for row in up :
+        for row in range(2,-1,-1) :
             for _ in range(3):
                 print("    ",end="")
-            for col in row :
+            for col in self.up[row] :
                 print(col,end=" ")
             print()
 
     
         for i in range(3):
-            print(left[i][0],left[i][1],left[i][2],end="   ")
-            print(front[i][0],front[i][1],front[i][2],end ="   ")
-            print(right[i][0],right[i][1],right[i][2],end = "   ")
-            print(back[i][0],back[i][1],back[i][2])
+            print(self.left[i][0],self.left[i][1],self.left[i][2],end="   ")
+            print(self.front[i][0],self.front[i][1],self.front[i][2],end ="   ")
+            print(self.right[i][0],self.right[i][1],self.right[i][2],end = "   ")
+            print(self.back[i][0],self.back[i][1],self.back[i][2])
 
-        for row in down :
+        for row in self.down :
             for _ in range(3):
                 print("   ",end=" ")
             for col in row :
